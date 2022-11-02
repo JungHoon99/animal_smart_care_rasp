6753db2d71304c80248b9e7e356575b3a5df885f
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2022-11-02 15:55:31 +0900

    Add files via upload

---
+++ client.py
@@ -0,0 +1,38 @@
+﻿import asyncio
+import base64
+
+import cv2
+import numpy as np
+import websockets
+import time
+
+
+async def start():
+    uri = "ws://3.36.68.46:53418"
+    async with websockets.connect(uri) as websocket:
+        Data = {'kind': 1, 'roomNumber' : "1234"}
+        await websocket.send(str(Data))
+        data = await websocket.recv()
+        if(data == "Connect Room"):
+             recvI = asyncio.create_task(recvImg(websocket))
+             sendC = asyncio.create_task(sendMessage(websocket))
+             await recvI
+             await sendC
+
+async def sendMessage(websocket):
+    while(1):
+        command = "hello Server"
+        await websocket.send(str({'who':"master","command":command}))
+        await asyncio.sleep(5.0)
+
+
+async def recvImg(websocket):
+    while cv2.waitKey(33) < 0:
+        get = await websocket.recv()
+        get = base64.b64decode(get)
+        encoded_img = np.fromstring(get, dtype = np.uint8)
+        img = cv2.imdecode(encoded_img, cv2.IMREAD_COLOR)
+        cv2.imshow("Video",img)
+
+
+asyncio.get_event_loop().run_until_complete(start())
---
+++ server.py
@@ -0,0 +1,30 @@
+﻿import asyncio
+import base64
+
+import cv2
+import websockets
+
+async def start():
+    uri = "ws://3.36.68.46:53418"
+    async with websockets.connect(uri) as websocket:
+        Data = {'kind': 0, 'roomNumber' : "1234"}
+        await websocket.send(str(Data))
+        send_t = asyncio.create_task(sendImg(websocket))
+        recvT = asyncio.create_task(recvCom(websocket))
+        await recvT
+        await send_t
+
+async def recvCom(websocket):
+    while(1):
+        get = await websocket.recv()
+        print(get)
+
+async def sendImg(websocket):
+    capture = cv2.VideoCapture(0)
+    while cv2.waitKey(33) < 0:
+            ret, frame = capture.read()
+            img = cv2.imencode('.jpg',frame)
+            
+            await websocket.send(base64.b64encode(img[1]).decode('utf-8'))
+
+asyncio.get_event_loop().run_until_complete(start())
